341075188ca6e50bebe3c7cfbb572c7a60493c05
Andrew Snisar <andrew.snisar@gmail.com>
2022-03-28 01:14:08 +0300

    created cart

--- src/App.js
+++ src/App.js
@@ -1,65 +1,93 @@
-import logo from './static/logo.png'
-import cartIcon from './static/cart.svg'
-import userIcon from './static/user.svg'
-import heartIcon from './static/heart.svg'
-import plusIcon from './static/plus.svg'
-import searchIcon from './static/search.svg'
-
-import favouriteEmptyIcon from './static/favourite-empty.svg'
-import favouriteFilledIcon from './static/favourite-filled.svg'
-
-import btnPlusIcon from './static/btn-plus.svg'
-import btnCheckedIcon from './static/btn-checked.svg'
-
-import sneaker1 from './static/sneakers/1.jpg'
-import sneaker2 from './static/sneakers/2.jpg'
-
 const App = () => {
   return (
     <div className="wrapper clear">
+      <div className="overlay">
+        <div className="drawer">
+          <h2 className="d-flex align-center justify-between  mb-30">
+            Корзина
+            <img className="removeBtn cu-p" src="/img/btn-remove.svg" alt="btnRemoveIcon" />
+          </h2>
+          <div className="items flex">
+            <div className="cartItem d-flex align-center mb-20">
+              <div
+                style={{ backgroundImage: 'url(/img/sneakers/1.jpg)' }}
+                className="cartItemImg"></div>
+              <div className="mr-20 flex">
+                <p className="mb-5">Мужские Кроссовки Nike Air Max 270</p>
+                <b>120 $</b>
+              </div>
+              <img className="removeBtn" src="/img/btn-remove.svg" alt="btnRemoveIcon" />
+            </div>
+            <div className="cartItem d-flex align-center mb-20">
+              <div
+                style={{ backgroundImage: 'url(/img/sneakers/1.jpg)' }}
+                className="cartItemImg"></div>
+              <div className="mr-20 flex">
+                <p className="mb-5">Мужские Кроссовки Nike Air Max 270</p>
+                <b>120 $</b>
+              </div>
+              <img className="removeBtn" src="/img/btn-remove.svg" alt="btnRemoveIcon" />
+            </div>
+          </div>
+          <div className="cartTotalBlock">
+            <ul>
+              <li>
+                <span>Итого:</span>
+                <div></div>
+                <b>1000 $</b>
+              </li>
+              <li>
+                <span>Налог 5%:</span>
+                <div></div>
+                <b>50$</b>
+              </li>
+            </ul>
+            <button className="greenButton">
+              Оформить заказ
+              <img src="/img/arrow.svg" alt="arrowIcon" />
+            </button>
+          </div>
+        </div>
+      </div>
 
       <header className="d-flex justify-between align-center p-40">
-
         <div className="d-flex align-center">
-          <img className="mr-15" width={40} height={40} src={logo} alt="logo"/>
+          <img className="mr-15" width={40} height={40} src="/img/logo.png" alt="logo" />
           <div>
             <h3 className="text-uppercase">Sneakers Shop</h3>
             <p className="opacity-5">Магазин лучших кроссовок</p>
           </div>
         </div>
-        
+
         <ul className="d-flex align-center">
           <li className="mr-30">
-            <img className="mr-10" width={18} height={18} src={cartIcon} alt="cartIcon"/>
+            <img className="mr-10" width={18} height={18} src="/img/cart.svg" alt="cartIcon" />
             <span>1205 $</span>
           </li>
           <li className="mr-30">
-            <img width={18} height={18} src={heartIcon} alt="heartIcon"/>
+            <img width={18} height={18} src="/img/heart.svg" alt="heartIcon" />
           </li>
           <li>
-            <img width={18} height={18} src={userIcon} alt="userIcon"/>
+            <img width={18} height={18} src="/img/user.svg" alt="userIcon" />
           </li>
         </ul>
-
       </header>
 
       <div className="content p-40">
-
         <div className="d-flex align-center justify-between mb-40">
           <h1>Все кроссовки</h1>
           <div className="search-block d-flex align-center">
-            <img width={18} height={18} src={searchIcon} alt="searchIcon"/>
-            <input placeholder="Поиск..."/>
+            <img width={18} height={18} src="/img/search.svg" alt="searchIcon" />
+            <input placeholder="Поиск..." />
           </div>
         </div>
 
         <div className="d-flex">
-
           <div className="card">
-            <div className="favourite">
-              <img src={favouriteEmptyIcon} alt="favourite"/>
+            <div className="favorite">
+              <img src="/img/favorite-empty.svg" alt="favorite" />
             </div>
-            <img width={133} height={112} src={sneaker1} alt="sneaker"/>
+            <img width={133} height={112} src="/img/sneakers/1.jpg" alt="sneaker" />
             <h5>Мужские Кроссовки Nike Blazer Mid Suede</h5>
             <div className="d-flex justify-between align-center">
               <div className="d-flex flex-column">
@@ -67,13 +95,13 @@ const App = () => {
                 <b>120 $</b>
               </div>
               <button className="button">
-                <img width={11} height={11} src={plusIcon} alt="plusIcon"/>
+                <img width={11} height={11} src="/img/plus.svg" alt="plusIcon" />
               </button>
             </div>
           </div>
 
           <div className="card">
-            <img width={133} height={112} src={sneaker2} alt="sneaker"/>
+            <img width={133} height={112} src="/img/sneakers/2.jpg" alt="sneaker" />
             <h5>Мужские Кроссовки Nike Blazer Mid Suede</h5>
             <div className="d-flex justify-between align-center">
               <div className="d-flex flex-column">
@@ -81,17 +109,14 @@ const App = () => {
                 <b>120 $</b>
               </div>
               <button className="button">
-                <img width={11} height={11} src={plusIcon} alt="plusIcon"/>
+                <img width={11} height={11} src="/img/plus.svg" alt="plusIcon" />
               </button>
             </div>
           </div>
-
         </div>
-
       </div>
-
     </div>
   );
-}
+};
 
 export default App;
